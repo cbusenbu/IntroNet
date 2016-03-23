d8789991cd4b77978fbcf245deb783577ff09392
Charles Busenburg <cbusenbu@gmail.com>
2016-03-23 15:53:09 -0400

    add Seed Project and Full template

---
+++ IntroNetMeteorSeed/client/config/router.js
@@ -0,0 +1,33 @@
+Router.configure({
+    layoutTemplate: 'mainLayout',
+    notFoundTemplate: 'notFound'
+
+});
+
+//
+// Example pages routes
+//
+
+Router.route('/pageOne', function () {
+    this.render('pageOne');
+});
+
+Router.route('/pageTwo', function () {
+    this.render('pageTwo');
+});
+
+Router.route('/login', function (){
+    this.render('login')
+    this.layout('blankLayout')
+
+});
+Router.route('/register', function(){
+    this.render('register')
+    this.layout('blankLayout')
+});
+Router.route('/', function () {
+    Router.go('pageOne');
+
+
+});
+
