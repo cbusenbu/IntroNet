7f95d3cb5000f921562d37778e04a0b89e69eb33
Charles Busenburg <cbusenbu@gmail.com>
2016-04-03 15:42:16 -0400

    UserCreate moved to the server

--- IntroNetMeteorSeed/collections/events.js
+++ IntroNetMeteorSeed/collections/events.js
@@ -1,25 +1,17 @@
 /**
  * Created by Chas on 4/2/16.
  */
-Events = new Mongo.collection('Events');
-
-Tacos.allow({
-    insert(){
-        return false;
-    },
-    update(){
-        return false;  
-    },
-    remove(){
-        return false;
-    }
+Events = new Mongo.Collection( 'events' );
+/*
+Events.allow({
+    insert() { return false; },
+    update() { return false; },
+    remove() { return false; }
 });
 
-Tacos.deny({
-    insert(){
-        return true;
-    },
-    update(){
-        
-    }
-})
+Events.deny({
+    insert() { return true; },
+    update() { return true; },
+    remove() { return true; }
+});
+    */
