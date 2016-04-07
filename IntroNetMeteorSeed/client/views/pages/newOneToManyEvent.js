06aef5e5ce0f33c7bed0c93dca123285eb5e88f3
Charles Busenburg <cbusenbu@gmail.com>
2016-04-06 21:47:55 -0400

    start new oneToManyEvent Hooks

--- IntroNetMeteorSeed/client/views/pages/newOneToManyEvent.js
+++ IntroNetMeteorSeed/client/views/pages/newOneToManyEvent.js
@@ -1,3 +1,25 @@
 /**
  * Created by Chas on 4/6/16.
  */
+Template.loginUserTemplate.events({
+    'submit form' (event, template){
+        event.preventDefault();
+
+        let eventObject = {
+
+            eventName: template.find("[name = 'eventName']").value,
+            eventLocation: template.find("[name = 'eventLocation']").value,
+            startDate: template.find("[name = 'startDate']").value,
+            endDate: template.find("[name = 'endDate']").value,
+            activityCount: template.find("[name = 'activityCount']").value,
+            activityLength: template.find("[name = 'activityLength']").value,
+            totalTime: template.find("[name = 'totalTime']").value,
+            breakLength: template.find("[name = 'breakLength']").value,
+            minParticipants: template.find("[name = 'minParticipants']").value,
+            sessionCount: template.find("[name = 'sessionCount']").value
+
+        };
+
+
+    }
+});
