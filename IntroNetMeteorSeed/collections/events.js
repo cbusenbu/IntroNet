3f0f4fdfb08f6b596d380f1f76a33b3754715f14
Charles Busenburg <cbusenbu@gmail.com>
2016-04-12 11:56:56 -0400

    basic change removing excess commented code

--- IntroNetMeteorSeed/collections/events.js
+++ IntroNetMeteorSeed/collections/events.js
@@ -5,16 +5,3 @@ if(Meteor.isServer){
     Events = new Mongo.Collection( 'events' );
 
 }
-/*
-Events.allow({
-    insert() { return false; },
-    update() { return false; },
-    remove() { return false; }
-});
-
-Events.deny({
-    insert() { return true; },
-    update() { return true; },
-    remove() { return true; }
-});
-    */
