5fd8d6bc6aba02bb6cebe4cfd71fb3dbc86550bf
Brandon Patterson <Brandon Patterson>
2016-04-20 22:58:18 -0400

    added third event parameter calculation

--- IntroNetMeteorSeed/client/controllers/newOneToManyEvent.js
+++ IntroNetMeteorSeed/client/controllers/newOneToManyEvent.js
@@ -2,6 +2,41 @@
  * Created by Chas on 4/6/16.
  */
 Template.newOneToManyEvent.events({
+    'click #calcThirdParam': function(event){
+        event.preventDefault();
+        var activityCount = document.getElementById('actvtyCount').value;
+        var activityLength = document.getElementById('actvtyLength').value;
+        var breakLength = document.getElementById('breakLength').value;
+        var totalTime = document.getElementById('totalTime').value;
+
+        if(breakLength==''){
+            console.log('break length cannot be empty')
+            document.getElementById('minParticipants').textContent = '';
+        }
+        else if(activityCount!='' && activityLength!=''){
+            totalTime = (Number(activityLength) + Number(breakLength)) * activityCount - breakLength;
+            document.getElementById('totalTime').value = totalTime;
+            document.getElementById('minParticipants').textContent = activityCount + " presenters, " + activityCount + " viewers";
+        }
+        else if(activityCount!='' && totalTime!=''){
+            activityLength = Math.floor((Number(totalTime) + Number(breakLength)) / activityCount - breakLength);
+            totalTime = (Number(activityLength) + Number(breakLength)) * activityCount - breakLength;
+            document.getElementById('actvtyLength').value = activityLength;
+            document.getElementById('totalTime').value = totalTime;
+            document.getElementById('minParticipants').textContent = activityCount + " presenters, " + activityCount + " viewers";
+        }
+        else if(activityLength!='' && totalTime!=''){
+            activityCount = Math.floor((Number(totalTime) + Number(breakLength))/(Number(activityLength) + Number(breakLength)));
+            totalTime = (Number(activityLength) + Number(breakLength)) * activityCount - breakLength;
+            document.getElementById('actvtyCount').value = activityCount;
+            document.getElementById('totalTime').value = totalTime;
+            document.getElementById('minParticipants').textContent = activityCount + " presenters, " + activityCount + " viewers";
+        }
+        else{
+            document.getElementById('minParticipants').textContent = '';
+        }
+    },
+
     'submit form' (event, template){
         event.preventDefault();
 
@@ -19,8 +54,6 @@ Template.newOneToManyEvent.events({
             activityLength: template.find("[name = 'activityLength']").value,
             totalTime: template.find("[name = 'totalTime']").value,
             breakLength: template.find("[name = 'breakLength']").value
-            
-
         };
 
         Meteor.call('insertEvent',eventObject,function(error,result){
--- IntroNetMeteorSeed/client/controllers/newOneToOneEvent.js
+++ IntroNetMeteorSeed/client/controllers/newOneToOneEvent.js
@@ -2,6 +2,41 @@
  * Created by Chas on 4/8/16.
  */
 Template.newOneToOneEvent.events({
+    'click #calcThirdParam': function(event){
+        event.preventDefault();
+        var activityCount = document.getElementById('actvtyCount').value;
+        var activityLength = document.getElementById('actvtyLength').value;
+        var breakLength = document.getElementById('breakLength').value;
+        var totalTime = document.getElementById('totalTime').value;
+
+        if(breakLength==''){
+            console.log('break length cannot be empty')
+            document.getElementById('minParticipants').textContent = '';
+        }
+        else if(activityCount!='' && activityLength!=''){
+            totalTime = (Number(activityLength) + Number(breakLength)) * activityCount - breakLength;
+            document.getElementById('totalTime').value = totalTime;
+            document.getElementById('minParticipants').textContent = Number(activityCount) + 2 - (activityCount % 2);
+        }
+        else if(activityCount!='' && totalTime!=''){
+            activityLength = Math.floor((Number(totalTime) + Number(breakLength)) / activityCount - breakLength);
+            totalTime = (Number(activityLength) + Number(breakLength)) * activityCount - breakLength;
+            document.getElementById('actvtyLength').value = activityLength;
+            document.getElementById('totalTime').value = totalTime;
+            document.getElementById('minParticipants').textContent = Number(activityCount) + 2 - (activityCount % 2);
+        }
+        else if(activityLength!='' && totalTime!=''){
+            activityCount = Math.floor((Number(totalTime) + Number(breakLength))/(Number(activityLength) + Number(breakLength)));
+            totalTime = (Number(activityLength) + Number(breakLength)) * activityCount - breakLength;
+            document.getElementById('actvtyCount').value = activityCount;
+            document.getElementById('totalTime').value = totalTime;
+            document.getElementById('minParticipants').textContent = Number(activityCount) + 2 - (activityCount % 2);
+        }
+        else{
+            document.getElementById('minParticipants').textContent = '';
+        }
+    },
+
     'submit form' (event, template){
         event.preventDefault();
 
