014f0d4a46d6a91d58d7930c25ca053ec39b9b4c
bjpatterson <bjpatterson@bsu.edu>
2016-04-29 17:08:59 -0400

    added an empty validation step to event creation

--- IntroNetMeteorSeed/client/controllers/newOneToManyEvent.js
+++ IntroNetMeteorSeed/client/controllers/newOneToManyEvent.js
@@ -1,6 +1,11 @@
 /**
  * Created by Chas on 4/6/16.
  */
+function isValidForm(){
+    console.log("Validation stage reached")
+    return true;
+};
+
 Template.newOneToManyEvent.events({
     'click #setSessionCount': function(event){
         event.preventDefault();
@@ -55,27 +60,27 @@ Template.newOneToManyEvent.events({
     'submit form' (event, template){
         event.preventDefault();
 
-        let eventObject = {
-            _id: new Mongo.ObjectID.valueOf(),
-            creatorID: Meteor.userId(),
-            createdAt: new Date(),
-            oneToOne: false,
-            oneToMany: true,
-            eventName: template.find("[name = 'eventName']").value,
-            eventLocation: template.find("[name = 'eventLocation']").value,
-            startDate: template.find("[name = 'startDate']").value,
-            endDate: template.find("[name = 'endDate']").value,
-            activityCount: template.find("[name = 'activityCount']").value,
-            activityLength: template.find("[name = 'activityLength']").value,
-            totalTime: template.find("[name = 'totalTime']").value,
-            breakLength: template.find("[name = 'breakLength']").value
-        };
+        if(isValidForm()){
+            let eventObject = {
+                _id: new Mongo.ObjectID.valueOf(),
+                creatorID: Meteor.userId(),
+                createdAt: new Date(),
+                oneToOne: false,
+                oneToMany: true,
+                eventName: template.find("[name = 'eventName']").value,
+                eventLocation: template.find("[name = 'eventLocation']").value,
+                startDate: template.find("[name = 'startDate']").value,
+                endDate: template.find("[name = 'endDate']").value,
+                activityCount: template.find("[name = 'activityCount']").value,
+                activityLength: template.find("[name = 'activityLength']").value,
+                totalTime: template.find("[name = 'totalTime']").value,
+                breakLength: template.find("[name = 'breakLength']").value
+            };
 
-        Meteor.call('insertEvent',eventObject,function(error,result){
-            Meteor.call('addEventToOwner', Meteor.userId(),result)
-        });
-        console.log('insert noted')
-
+            Meteor.call('insertEvent',eventObject,function(error,result){
+                Meteor.call('addEventToOwner', Meteor.userId(),result)
+            });
+            console.log('insert noted')
+        }
     }
-
 });
--- IntroNetMeteorSeed/client/controllers/newOneToOneEvent.js
+++ IntroNetMeteorSeed/client/controllers/newOneToOneEvent.js
@@ -1,7 +1,10 @@
 /**
  * Created by Chas on 4/8/16.
  */
-
+function isValidForm(){
+    console.log("Validation stage reached")
+    return true;
+};
 
 Template.newOneToOneEvent.events({
     'click #setSessionCount': function(event){
@@ -57,27 +60,30 @@ Template.newOneToOneEvent.events({
     'submit form' (event, template){
         event.preventDefault();
 
-        let eventObject = {
-            creatorID: Meteor.userId(),
-            createdAt: new Date(),
-            oneToOne: true,
-            oneToMany: false,
-            eventName: template.find("[name = 'eventName']").value,
-            eventLocation: template.find("[name = 'eventLocation']").value,
-            startDate: template.find("[name = 'startDate']").value,
-            endDate: template.find("[name = 'endDate']").value,
-            activityCount: template.find("[name = 'activityCount']").value,
-            activityLength: template.find("[name = 'activityLength']").value,
-            totalTime: template.find("[name = 'totalTime']").value,
-            breakLength: template.find("[name = 'breakLength']").value
+        if(isValidForm()){
+
+            let eventObject = {
+                creatorID: Meteor.userId(),
+                createdAt: new Date(),
+                oneToOne: true,
+                oneToMany: false,
+                eventName: template.find("[name = 'eventName']").value,
+                eventLocation: template.find("[name = 'eventLocation']").value,
+                startDate: template.find("[name = 'startDate']").value,
+                endDate: template.find("[name = 'endDate']").value,
+                activityCount: template.find("[name = 'activityCount']").value,
+                activityLength: template.find("[name = 'activityLength']").value,
+                totalTime: template.find("[name = 'totalTime']").value,
+                breakLength: template.find("[name = 'breakLength']").value
 
 
-        };
+            };
 
-        Meteor.call('insertEvent',eventObject,function(error,result){
-            Meteor.call('addEventToOwner', Meteor.userId(),result)
-        });
-        console.log('insert noted')
+            Meteor.call('insertEvent', eventObject, function (error, result) {
+                Meteor.call('addEventToOwner', Meteor.userId(), result)
+            });
+            console.log('insert noted')
+        }
 
     }
 
